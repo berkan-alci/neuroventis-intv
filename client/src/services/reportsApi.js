61707495ff8ba0fd69e5eaceb37c9522fc6a70b8
Berkan Alci <alci.berkan@gmail.com>
2022-05-13 20:46:44 +0200

    srv url fix

--- client/src/services/reportsApi.js
+++ client/src/services/reportsApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseUrl = 'http://localhost:5000/report'
+const baseUrl = 'http://nv-interview.com/report'
 
 const createRequest = (url) => ({ url })
 
